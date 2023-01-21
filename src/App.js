c8e085ab5668d2f368ef026e141388c5506c5844
touraye <touraye07@gmail.com>
2023-01-21 14:57:40 +0000

    updated app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login';
@@ -9,12 +7,12 @@ import Users from './pages/pagination/UserPagination'
 import Chart from './pages/Chart';
 import Account from './pages/Account';
 import NotFound from './pages/NotFound';
+import Footer from './components/Footer'
 
 function App() {
   return (
 		<div className='container'>
-			<Router>
-				{/* <Header /> */}
+			<Router>				
 				<main className='main'>
 					<Routes>
 						<Route path='/' element={<Dashboard />} />
